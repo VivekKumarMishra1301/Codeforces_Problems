9c525e9ecfde5072ec3aa56e08dbb99900872092
Vivek <vivekkmishra1301@gmail.com>
2023-03-22 01:18:07 +0530

    Format of codeforces coding

---
+++ Format_Codeforces.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+void precal()
+{
+}
+void solve()
+{
+}
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    cout << fixed;
+    cout << setprecision(10);
+    precal();
+    int t = 1;
+    // cin>>t;
+    for (int i = 1; i <= t; i++)
+    {
+        solve();
+    }
+}
